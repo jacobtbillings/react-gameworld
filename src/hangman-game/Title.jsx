c914952b792863d6d5dc013bb89162b6231a56f9
JTBillings <jacobtbillings@gmail.com>
2023-04-21 09:36:00 -0400

    hangman subtitle

--- src/hangman-game/Title.jsx
+++ src/hangman-game/Title.jsx
@@ -4,6 +4,7 @@ export default function Title () {
     return (
         <div className='page-title-hangman'>
             <h1>Hangman: The Office</h1>
+            <p>Fill in the episode's title to win!</p>
         </div>
     )
 }
